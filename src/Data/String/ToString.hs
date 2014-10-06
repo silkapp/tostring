fea94b1536cd94ac19c20884fc617f30b94d3416
Adam Bergmark <adam@bergmark.nl>
2014-10-06 21:30:26 +0200

    Add ToString instance for case-insensitive

--- src/Data/String/ToString.hs
+++ src/Data/String/ToString.hs
@@ -2,10 +2,9 @@
     FlexibleInstances
   , TypeSynonymInstances
   #-}
-module Data.String.ToString
-  ( ToString (..)
-  ) where
+module Data.String.ToString (ToString (..)) where
 
+import Data.CaseInsensitive (CI, foldedCase)
 import qualified Data.ByteString.Lazy.UTF8 as LBU
 import qualified Data.ByteString.UTF8      as SBU
 import qualified Data.Text                 as ST
@@ -28,3 +27,6 @@ instance ToString ST.Text where
 
 instance ToString LT.Text where
   toString = LT.unpack
+
+instance ToString s => ToString (CI s) where
+  toString = toString . foldedCase
