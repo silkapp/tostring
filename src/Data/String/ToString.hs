9fe0e5006f7f33606c55220f8301d765c688b0fc
Simon Hengel <sol@typeful.net>
2014-10-15 18:38:58 +0800

    Add instances for Bool and some numeric types

--- src/Data/String/ToString.hs
+++ src/Data/String/ToString.hs
@@ -5,6 +5,8 @@
   #-}
 module Data.String.ToString (ToString (..)) where
 
+import Data.Int
+import Data.Word
 import Data.CaseInsensitive (CI, foldedCase)
 import qualified Data.ByteString.Lazy.UTF8 as LBU
 import qualified Data.ByteString.UTF8      as SBU
@@ -15,6 +17,21 @@ class ToString a where
   toString :: a -> String
   default toString :: Show a => a -> String
   toString = show
+
+instance ToString Bool
+instance ToString Float
+instance ToString Double
+instance ToString Int
+instance ToString Int8
+instance ToString Int16
+instance ToString Int32
+instance ToString Int64
+instance ToString Integer
+instance ToString Word
+instance ToString Word8
+instance ToString Word16
+instance ToString Word32
+instance ToString Word64
 
 instance ToString String where
   toString = id
